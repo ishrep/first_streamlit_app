7d22f5b30fe618340d5217360c5f813fdd16f8e3
Ishan Agarwal <53993701+ishrep@users.noreply.github.com>
2023-10-06 23:42:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("hello world wake its morning!")
 
@@ -6,3 +7,7 @@ streamlit.text("test1")
 streamlit.text("test2")
 
 streamlit.header("😂😂haslo bhai")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
