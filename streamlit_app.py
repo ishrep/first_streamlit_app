e6cb336ad0dfd23a1b3951fc5840787dc90240f2
Ishan Agarwal <53993701+ishrep@users.noreply.github.com>
2023-10-20 10:23:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,4 +33,5 @@ streamlit.dataframe(my_data_row)
 
 
 fruit_choice = streamlit.text_input('What would you like to add?')
-streamlit.write('Thanks for adding ', fruit_choice)
+if(fruit_choice != ''):
+  streamlit.write('Thanks for adding ', fruit_choice)
